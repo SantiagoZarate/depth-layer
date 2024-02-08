d42b9eba3c2885ee7d92265a63aa0f9d16cb5d4a
Santiago Zarate <santiago2001zarate@gmail.com>
2024-02-07 21:10:36 -0300

    add: code syntax

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,8 +1,19 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
+import AutoImport from 'astro-auto-import'
+import mdx from '@astrojs/mdx'
+
+import MDXCodeBlocks, { mdxCodeBlockAutoImport } from 'astro-mdx-code-blocks'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), react(), AutoImport({
+    imports: [
+      mdxCodeBlockAutoImport('src/components/CodeSnippet.astro')
+    ]
+  }),
+  MDXCodeBlocks(),
+  mdx()],
 });
---
+++ web/public/snippetCode.js
@@ -0,0 +1,3 @@
+const hi = "Hello world"
+
+export default hi
