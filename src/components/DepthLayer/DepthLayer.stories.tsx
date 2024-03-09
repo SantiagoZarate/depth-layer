083668f7ef7787db8373d3418a786b19fcc644d3
Santiago Zarate <santiago2001zarate@gmail.com>
2024-03-09 20:24:53 -0300

    fix: remove rounded property on depthLayer

--- src/components/DepthLayer/DepthLayer.stories.tsx
+++ src/components/DepthLayer/DepthLayer.stories.tsx
@@ -1,27 +1,23 @@
-import type { Meta, StoryFn, StoryObj } from '@storybook/react';
-import React from 'react';
+import React from "react";
+import type { Meta, StoryFn, StoryObj } from "@storybook/react";
 
-import { DepthLayer } from './DepthLayer';
+import { DepthLayer } from "./DepthLayer";
 
 const meta = {
-  id: 'depthlayer-default--docs',
-  title: 'DepthLayer',
+  id: "depthlayer-default--docs",
+  title: "DepthLayer",
   component: DepthLayer,
   parameters: {
-    layout: 'centered',
+    layout: "centered",
   },
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   argTypes: {
     hoverable: {
-      control: 'boolean'
+      control: "boolean",
     },
     clickable: {
-      control: 'boolean'
+      control: "boolean",
     },
-    round: {
-      options: ['full', 'none'],
-      control: { type: 'radio' }
-    }
   },
 } satisfies Meta<typeof DepthLayer>;
 
@@ -31,18 +27,20 @@ type Story = StoryObj<typeof meta>;
 const Template: StoryFn<typeof DepthLayer> = (args) => (
   <DepthLayer {...args}>
     <img
-      className='w-32 h-32 object-cover'
+      id="img"
+      className="w-32 h-32 object-cover rounded-full"
       src="https://pics.craiyon.com/2023-07-19/372d10a1044941c0a471ac071b686a6a.webp"
-      alt="" />
+      alt=""
+    />
   </DepthLayer>
-)
+);
 
-export const Default: Story = Template.bind({})
+export const Default: Story = Template.bind({});
 Default.args = {
-  variant: 'default'
+  variant: "default",
 };
 
-export const Inner: Story = Template.bind({})
+export const Inner: Story = Template.bind({});
 Inner.args = {
-  variant: 'inner'
+  variant: "inner",
 };
